24f5f5950762c508ed1d062c71c1e610555dbac4
Jonathan Navarro <jonathan@ranto.cl>
2021-08-07 22:53:29 -0400

    Primer commit

---
+++ src/test/java/cl/pruebamaven/proyecto/PruebaMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.pruebamaven.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
